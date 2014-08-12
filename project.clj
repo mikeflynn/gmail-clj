f9761a2c69ecb5dd08a0712116be7fe905c1373b
Mike Flynn <mfflynn@gmail.com>
2014-08-12 14:53:16 -0700

    master: Updated url in project file.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject gmail-clj "0.6"
+(defproject gmail-clj "0.6.1"
   :description "A simple Clojure library to interface with the GMail API (read: not imap)"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/mikeflynn/gmail-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
